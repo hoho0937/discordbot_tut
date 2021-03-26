c0b405758e6fea850e0c8f22a150735840496a13
BBLG <eric558994@gmail.com>
2021-03-27 01:18:43 +0800

    delete token
    delete token

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ bot = commands.Bot(command_prefix='[')
 async def on_ready():
     print(">> bot is online <<")
 
-bot.run('ODI1MDM2NjU5OTQ2NTUzMzg1.YF4FRA.7ImBJStIw6IgIJqv112qRKQ6rJY')
+bot.run('My_bot_TOKEN')
