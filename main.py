8c2f1827814bacdd981c586f77e866020530c5d8
BBLG <eric558994@gmail.com>
2021-03-27 15:43:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import discord
 from discord.ext import commands
+import json
 
-bot = commands.Bot(command_prefix='[')
+with open('setting.json','r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='[', intents=intents)
+
+client = discord.Client(intents=intents)
 
 @bot.event
 async def on_ready():
     print(">> bot is online <<")
 
-bot.run('My_bot_TOKEN')
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(825240051817906176)
+    await channel.send(F'{member} join!')
+    print(F'{member} join!')
+
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(825240084298858506)
+    await channel.send(F'{member} join!')
+    print(F'{member} leave!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(F'{round(bot.latency * 1000)}(ms)')
+
+bot.run(jdata['TOKEN'])
